2bac4b8a03cf8d6767c04c1eb56640b2650fb1b9
ClaraMelaniePelozo <clara.pelozo@est.fi.uncoma.edu.ar>
2025-04-14 14:40:50 -0300

    Agregue carpetas que faltaban

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css'
-
+import './App.css';
+import Home from './pages/Home/home';
 function App() {
   return (
-    <>
+    <div>
       <h1>TP React | PWA</h1>
-    </>
+      <Home />
+    </div>
   )
 }
 
